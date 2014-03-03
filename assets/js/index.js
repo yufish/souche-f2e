d8c0a4a662d17b444ccd7ee62d1d523e0f0e8bd1
xinyu198736 <xinyu198736@gmail.com>
2014-03-03 20:02:04 +0800

    首页项目准备

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,26 +1,17 @@
-require.config({
-	baseUrl: "/assets/js"
-});
-require(['souche/select'], function (Select){
-　　　　
-　　});
+
 Souche.Index = (function(){
 	var config = {
-		api_isLogin:'',
-		api_saveBuyInfo:'',
-		api_detail:'',
-		api_changeCars:''
+		
 	};
 	return {
 		init:function(_config){
-			Souche.Util.mixin(config,_config);
-			
+			$.extend(config,_config);
 		}
-
 	};
 	
 })();
 
-define(function (){
-　　　　return Souche.Index;
-　　});
+define(['souche/custom-select'], function (CustomSelect){
+	var brandSelect = new CustomSelect("brand_select");
+	return Souche.Index;
+});
