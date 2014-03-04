0ed2b13a7eb8d481c55990d4c386c92233c97c9a
xinyu198736 <xinyu198736@gmail.com>
2014-03-04 16:22:47 +0800

    dd

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,11 +6,34 @@ Souche.Index = (function(){
 	return {
 		init:function(_config){
 			$.extend(config,_config);
-
+			//sidebar自动顶住
+			var contentTop = $("#content").offset().top;
+			var contentHeight = $("#content").height();
+			var sidebarHeight = $("#side_bar").height();
+			$(window).on("scroll",function(e){
+				var windowTop = $(window).scrollTop();
+				//顶部对齐
+				if(windowTop>(contentTop-10)){
+					$("#side_bar").css({
+						top:windowTop-contentTop+10
+					})
+				}else{
+					$("#side_bar").css({
+						top:0
+					})
+				}
+				//底部对其
+				if(sidebarHeight+windowTop>contentTop+contentHeight){
+					$("#side_bar").css({
+						top:contentHeight-sidebarHeight
+					})
+				}
+			})
 			$('.down-counter').each(function(){
 	      var $this = $(this);
 	      downCounter($this);
 	    });
+
 		}
 	};
 	
