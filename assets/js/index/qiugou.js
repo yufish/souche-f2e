b5e8975d1ceb49543cc276a03712949c2f0c3978
xinyu198736 <xinyu198736@gmail.com>
2014-03-04 15:38:57 +0800

    dd

--- assets/js/index/qiugou.js
+++ assets/js/index/qiugou.js
@@ -1,5 +1,20 @@
 define(['souche/custom-select','souche/select'], function (CustomSelect,Select){
   var brandSelect,seriesSelect,priceLowSelect,priceHighSelect,ageSelect,modelSelect;
+  var brandSort = function(data){
+    var zimu = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    var obj = {}
+    for(var i in data){
+      var brand = data[i]
+      var firstword = brand.name.charAt(0).toUpperCase();
+      if(!obj[firstword]){
+        obj[firstword] = []
+      }
+      brand.name = brand.name.substr(2,brand.name.length)
+      obj[firstword].push(brand);
+    }
+
+    return obj;
+  }
   return {
     init:function(){
       brandSelect = new CustomSelect("brand_select",{
@@ -34,10 +49,17 @@ define(['souche/custom-select','souche/select'], function (CustomSelect,Select){
         },
         success:function(data){
           var html = "";
-
-          for(var i in data.items){
-            var item = data.items[i];
-            html+=('<a href="#" data-value="'+item.code+'" class="option"><input type="checkbox" class="hidden"/><span class="value">'+item.name+'</span></a>');
+          data = brandSort(data.items);
+          for(var i in data){
+            var b = data[i];
+            var name = i;
+            html+="<div data-name='"+name+"' class='clearfix'><div class='word-title'>"+name+"</div>"
+            for(var n =0;n<b.length;n++){
+              var brand = b[n]
+              html+=('<a href="#" data-value="'+brand.code+'" class="option"><input type="checkbox" class="hidden"/><span class="value">'+brand.name+'</span></a>');
+            }
+            html+="</div>"
+            
           }
           brandSelect.addOptions(html)
         },  
@@ -55,12 +77,16 @@ define(['souche/custom-select','souche/select'], function (CustomSelect,Select){
         console.log(data)
         self._addSeries(data.key)
         //选中了某品牌
-      }).on("unselect",function(){
+      }).on("unselect",function(e,data){
+        console.log(data)
         self._removeSeries(data.key)
         //取消选中某品牌，删除其所拥有的车系列表
       })
     },
     _addSeries:function(brandCode){
+      if($("#series_select .sc-select-list div[data-brandid="+brandCode+"]").length){
+        return;
+      }
       $.ajax({
         url:"/demo/yutou/index/series.json",//contextPath+"/pages/dicAction/loadRootLevelForCar.json",
         dataType:"json",
@@ -74,7 +100,7 @@ define(['souche/custom-select','souche/select'], function (CustomSelect,Select){
           for(var i in data.codes){
             var b = data.codes[i];
             var name = i;
-            html+="<div data-name='"+name+"' data-brandId='"+brandCode+"'>"
+            html+="<div data-name='"+name+"' data-brandid='"+brandCode+"' class='clearfix'><div class='brand-title'>"+name+"</div>"
             for(var n =0;n<b.length;n++){
               var series = b[n]
               html+=('<a href="#" data-value="'+series.code+'" class="option"><input type="checkbox" class="hidden"/><span class="value">'+series.name+'</span></a>');
@@ -93,7 +119,8 @@ define(['souche/custom-select','souche/select'], function (CustomSelect,Select){
       });
     },
     _removeSeries:function(brandCode){
-
+      console.log($("#series_select .sc-select-list div[data-brandid="+brandCode+"]"))
+      $("#series_select .sc-select-list div[data-brandid="+brandCode+"]").remove();
     }
   };
 });
