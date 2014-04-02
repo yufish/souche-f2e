6a335f03f434f352fe7ea1abadf288730f520ec1
zilong <jzlxiaohei@163.com>
2014-04-02 14:37:52 +0800

    h5-2

--- assets/js/mobile/cookieUtil.js
+++ assets/js/mobile/cookieUtil.js
@@ -1,4 +1,34 @@
-!function(exports,undefined){
-	
+var CookieUtil = function(){
+	function initCookie(){
+		var cookie = document.cookie;
+		cookie.split(';').forEach(function(item){
+			var kvPair = item.split('=');
+			var key = kvPair[0];
+			var value = kvPair[1];
+			cookieMap[key.trim()] = value.trim();
+		})
+	}
+	var cookieMap = {};
+	return {
+		init:function(){
+			initCookie();
+		},
+		getCookie:function(cName){
+			return cookieMap[cName];
+		}
+	}
+}()
 
-}(window)
+var CompletePhone =function($input){
+	var phoneReg = /^1[3458][0-9]{9}$/;
+	var username = CookieUtil.getCookie('username')
+	if(phoneReg.test(username)){
+		$input.val(username);
+		return;
+	}
+	var noregisteruser = CookieUtil.getCookie('noregisteruser');
+	if(phoneReg.test(noregisteruser)){
+		$input.val(username);
+		return;
+	}
+}
