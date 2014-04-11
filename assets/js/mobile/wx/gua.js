09fe57bb0f3219ce05a5defdb849b24664a20182
zilong <jzlxiaohei@163.com>
2014-04-11 17:12:50 +0800

    wx pic

--- assets/js/mobile/wx/gua.js
+++ assets/js/mobile/wx/gua.js
@@ -1,6 +1,5 @@
-﻿
+﻿var lottery = new Lottery('canvas-container','#d7d7d7','刮奖区','color', 270, 140);
 $('.bind').one('click',function(){
-	var lottery = new Lottery('canvas-container','#d7d7d7','刮奖区','color', 270, 140, drawPercent);
 	lottery.init('刮奖区','text');
 	$('#canvas-container .before-bind').remove();
 	$('#canvas-container').prepend($('.fixed-award').removeClass('hidden'));
