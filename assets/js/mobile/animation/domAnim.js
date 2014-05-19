471b113f52a0bc1c52a753852ae6243b23ab8ab2
zilong <jzlxiaohei@163.com>
2014-05-19 13:38:52 +0800

    detail.css bug fix

--- assets/js/mobile/animation/domAnim.js
+++ assets/js/mobile/animation/domAnim.js
@@ -337,6 +337,12 @@ var animateFuncs_s2 = function () {
             function drawCircle() {
                 if (deg > 450) {
                     clearInterval(drawHandler);
+
+                    ctx.clearRect(0, 0, 110, 110);
+                    ctx.beginPath();
+                    ctx.arc(55, 55, 50, startR, deg * fenMu);
+                    ctx.stroke();
+
                     repeatNext();
                     return;
                 } else {
@@ -480,6 +486,12 @@ var animateFuncs_s3 = function () {
             function drawCircle() {
                 if (deg > 450) {
                     clearInterval(drawHandler);
+
+                    ctx.clearRect(0, 0, 110, 110);
+                    ctx.beginPath();
+                    ctx.arc(55, 55, 50, startR, deg * fenMu);
+                    ctx.stroke();
+
                     repeatNext();
                     return;
                 } else {
