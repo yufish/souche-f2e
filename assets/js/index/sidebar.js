ccb367e008aaf9c96397e019d72737e1d1832cee
pe_Jagten <p19911227@163.com>
2014-05-29 17:34:37 +0800

    address

--- assets/js/index/sidebar.js
+++ assets/js/index/sidebar.js
@@ -83,7 +83,7 @@ Souche.Sidebar = (function() {
         var Q_Buy_active = false;
         $(window).scroll(function() {
             if ($(window).scrollTop() > 0) {
-                $("#toTop").removeClass("hidden");
+
                 $("#toTop").show("slow");
             } else {
                 $("#toTop").hide("slow");
