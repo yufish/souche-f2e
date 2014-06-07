c42d8246aa18c03c3742e490d0a290be065674d2
zilong <jzlxiaohei@163.com>
2014-06-07 14:08:56 +0800

    for_app

--- assets/js/mobile/filter/filtercar-scroll.js
+++ assets/js/mobile/filter/filtercar-scroll.js
@@ -19,3 +19,32 @@ $(document).ready(function(){
     //window.popupBtnPositioned = popupBtnPositioned;
 });
 */
+function map(){
+    emit(this.data.keyword,{tags:[this.data.usertag],count:1});
+}
+
+function reduce(key,values){
+    var tagsFlag  = {};
+    var tags = [];
+    for(var i = 0;i<values.length;i++){
+        var tagItem = values[i].tags;
+        for(var j in tagItem){
+            var flag = tagItem[j]
+            if(!( flag in tagsFlag)){
+                tagsFlag[flag]=true;
+                tags.push(flag);
+            }
+        }
+    }
+    return {tags:tags,count:tags.length}
+}
+
+db.runCommand({
+    mapReduce:'trackModel',
+    map:map,
+    reduce:reduce,
+    out:{replace:'tempCol'},
+    query:{typeid:'TYPE_WEB_PAGE_SEARCH',date:{
+        '$gte':new Date("2014/06/03"),'$lte':new Date('2014/06/04')
+    }}
+})
