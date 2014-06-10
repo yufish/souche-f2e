feafd29605ba02fb1ea190b7c7a0a35b3e2aa46a
xinyu198736 <xinyu198736@gmail.com>
2014-06-10 17:07:03 +0800

    detail

--- assets/js/detail/detail_common.js
+++ assets/js/detail/detail_common.js
@@ -24,7 +24,7 @@
         $body: $("body, html"),
         $winTop: "",
         mainTop: $("#detail_main").offset().top - 50,
-        sumTop: $("#onsale_sum").offset().top - 50,
+        // sumTop: $("#onsale_sum").offset().top - 50,
         paraTop: $("#detail_para").offset().top - 50,
         recordTop: $("#onsale_record").offset().top - 50,
         navVisible: false,
