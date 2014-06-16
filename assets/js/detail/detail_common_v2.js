75e9b87faede06870f533bf7e5ca54f83ba660f8
Dpangzi <qjb3013650@163.com>
2014-06-16 19:36:52 +0800

    ddetail

--- assets/js/detail/detail_common_v2.js
+++ assets/js/detail/detail_common_v2.js
@@ -395,22 +395,86 @@
             }
         })
     }
+
+    var submitFav = function() {
+        $.ajax({
+            url: SaleDetailConfig.api_saveFavorite,
+            data: {
+                phone: $("#fav-phone").val(),
+                carType: SaleDetailConfig.carType,
+                carId: SaleDetailConfig.carId
+            },
+            dataType: "json",
+            type: "post",
+            success: function(data) {
+                if (data.errorMessage) {
+                    alert(data.errorMessage)
+                } else {
+                    //$('#shoucang-popup').removeClass('hidden');
+                    var favPos = $("#J_shoucang").offset();
+                    $("<div class='icon-fei'></div>").css({
+                        left: favPos.left + 7,
+                        top: favPos.top + 7
+                    })
+                        .appendTo(document.body)
+                        .animate({
+                            left: $(".sidebar").offset().left + 10,
+                            top: $(".sidebar").offset().top + 10,
+                            opacity: 0
+                        }, 700, function() {
+                            $(".collectside").addClass("flash")
+                            setTimeout(function() {
+                                $(".collectside").removeClass("flash")
+                            }, 500)
+                        })
+                    $("#fav-popup").addClass("hidden")
+                    $(".wrapGrayBg").hide();
+                    $("#J_shoucang label").html('已收藏')
+                    $("#J_shoucang").attr('value', '1').addClass("faved");
+                    var num = $('#J_car_favorite').html();
+                    $('#J_car_favorite').html(parseInt(num) + 1);
+                    doubleClickFlag = false;
+                }
+            }
+        })
+    }
+
+    var cancelFavSubmit = function() {
+        $.ajax({
+            url: SaleDetailConfig.api_delFavorite,
+            data: {
+                carId: SaleDetailConfig.carId //$(self).attr("data-carid")
+            },
+            dataType: "json",
+            type: "post",
+            success: function(data) {
+                if (data.errorMessage) {
+                    alert(data.errorMessage)
+                } else {
+                    $("#J_shoucang label").html('收藏')
+                    $("#J_shoucang").removeClass("faved");
+                }
+            }
+        })
+    }
     $("#J_shoucang").live('click', function(e) {
 
         e.preventDefault();
 
         if ($(this).hasClass("faved")) {
             return;
-        }
-        Souche.checkPhoneExist(function(is_login) {
-            if (is_login) {
+        }else{
+            Souche.checkPhoneExist(function(is_login) {
+                if (is_login) {
 
-                submitFav();
-            } else {
-                $("#fav-popup").removeClass("hidden")
-                $(".wrapGrayBg").show();
-            }
-        })
+                    submitFav();
+                } else {
+                    $("#fav-popup").removeClass("hidden")
+                    $(".wrapGrayBg").show();
+                }
+            })
+        }
+
 
     });
     $("#fav-form").on("submit", function(e) {
