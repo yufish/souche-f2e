ec0bf125374146e3e23e127f9fd80916eae9c21f
zilong <jzlxiaohei@163.com>
2014-06-20 11:08:36 +0800

    cheniu

--- assets/js/mobile/guwen/scroll.js
+++ assets/js/mobile/guwen/scroll.js
@@ -2,7 +2,17 @@
  * Created by zilong on 2014/6/19.
  */
 var $win = $(window);
-var sc = $('#series-container');
+var $sCtn = $('#series-container');
+var $bCtn = $('#brand-icons-container')
+var $sb = $('#selected-brand');
+var $ss =$('#selected-series');
 $win.scroll(function(){
-
+    var offset = $bCtn.offset();
+    var winTop = $win.scrollTop();
+    if(offset.top<winTop){
+        var sbHeight = $sb.height();
+        $sb.css({height:sbHeight});
+    }else{
+        $sb.css({height:'auto'})
+    }
 })
