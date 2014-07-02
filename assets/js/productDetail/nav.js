a893e33f3415b69155ace62a779542dd5ad53563
Dpangzi <qjb3013650@163.com>
2014-07-02 09:49:48 +0800

    jsproduct

---
+++ assets/js/productDetail/nav.js
@@ -0,0 +1,52 @@
+/**
+ * Created by Administrator on 2014/7/1.
+ */
+define(function()
+{
+    var navControl = {};
+    var navTabTop = $("#productDetailInfo .nav").offset().top;
+
+    var init = function() {
+        $("#productDetailInfo .nav li").click(function () {
+            var dataID = $(this).attr("data-id");
+            $("#productDetailInfo>div").addClass("hidden");
+            $("#productDetailInfo>div[data-id='" + dataID + "']").removeClass("hidden");
+
+            $(this).addClass("active").siblings().removeClass("active");
+
+            if ($("#productDetailInfo .nav").css("position") === "fixed") {
+                $('html,body').animate(
+                    {
+                        scrollTop: navTabTop + "px"
+                    }
+                    , 1000);
+            }
+        });
+
+        $(window).scroll(function () {
+            var winTop = $(window).scrollTop();
+
+            if (winTop > navTabTop) {
+                $("#productDetailInfo .nav").css({
+                    position: "fixed",
+                    top: 0,
+                    width: "100%",
+                    zIndex: 1000
+                })
+            } else {
+                $("#productDetailInfo .nav").css({
+                    position: "relative"
+                })
+            }
+            var height = $("#productDetailInfo").height();
+            if (winTop > navTabTop + height - 40) {
+                $("#productDetailInfo .nav").css({
+                    position: "relative"
+                })
+            }
+        });
+    }
+
+    navControl.init = init;
+    return navControl;
+});
