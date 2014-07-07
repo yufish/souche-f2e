ae8927207154cb302870fb062397c9f6156d536d
Dpangzi <qjb3013650@163.com>
2014-07-07 17:43:47 +0800

    indexhtml

--- assets/js/index_v2.js
+++ assets/js/index_v2.js
@@ -1,9 +1,16 @@
 define([""],function(){
     var config = {};
+
+    var _bind = function()
+    {
+
+    }
+
     return {
         init:function(_config){
             $.extend(config,_config);
 
+            _bind();
         }
     }
 });
