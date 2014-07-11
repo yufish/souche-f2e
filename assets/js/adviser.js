955161356c532a33d0c72a2b09396c325997e456
pe_Jagten <p19911227@163.com>
2014-07-11 16:00:04 +0800

    adviser

---
+++ assets/js/adviser.js
@@ -0,0 +1,120 @@
+require(['souche/util/action-list'], function(ActionList) {
+    var defaultDist = $(window).height() * 0.7;
+
+    Souche.Util.appear("#aiche", function() {
+        var data1 = [
+            [0,
+                function() {
+                    $(".pic-1-4").animate({
+                        height: 405
+                    }, 2000)
+                }
+            ],
+            [2500,
+                function() {
+                    $(".pic-1-5").animate({
+                        height: 580
+                    }, 2500)
+                }
+            ],
+            [4000,
+                function() {
+                    $(".pic-1-6").animate({
+                        height: 627
+                    }, 2500)
+                }
+            ]
+        ]
+        ActionList(data1, function() {})
+    }, defaultDist);
+
+    Souche.Util.appear("#test", function() {
+        var data = [
+            [1000,
+                function() {
+                    $(".pic-2-1").animate({
+                        opacity: 0
+                    }, 1000)
+                    $(".car-1").animate({
+                        opacity: 0
+                    }, 1000)
+                    $(".car-5").animate({
+                        opacity: 0
+                    }, 1000)
+                    $(".text-1").animate({
+                        opacity: 0
+                    }, 1000)
+                }
+            ],
+            [2000,
+                function() {
+                    $(".pic-2-2").animate({
+                        opacity: 0,
+                    }, 1000)
+                    $(".car-2").animate({
+                        opacity: 0
+                    }, 1000)
+                    $(".car-4").animate({
+                        opacity: 0
+                    }, 1000)
+                    $(".text-2").animate({
+                        opacity: 0
+                    }, 1000)
+                }
+            ]
+        ]
+        ActionList(data, function() {})
+    }, defaultDist);
+    Souche.Util.appear("#bargain", function() {
+        var data = [
+            [0,
+                function() {
+                    $(".pic-3-2-1").animate({
+                        height: 353
+                    }, 1000)
+                }
+            ],
+            [1200,
+                function() {
+                    $(".pic-3-2-2").animate({
+                        height: 375
+                    }, 1000)
+                }
+            ],
+            [1800,
+                function() {
+                    $(".pic-3-2-3").animate({
+                        height: 369
+                    }, 1000)
+                }
+            ],
+            [2200,
+                function() {
+                    $(".pic-3-2-4").animate({
+                        height: 355
+                    }, 1000)
+                }
+            ],
+            [2800,
+                function() {
+                    $(".pic-3-2-5").animate({
+                        height: 413
+                    }, 1000)
+                }
+            ],
+        ]
+        ActionList(data, function() {})
+    }, defaultDist);
+    Souche.Util.appear("#staging", function() {
+        var data = [
+            [0,
+                function() {
+                    $(".pic-4-2").animate({
+                        width: 291
+                    }, 1000)
+                }
+            ]
+        ]
+        ActionList(data, function() {})
+    }, defaultDist);
+})
