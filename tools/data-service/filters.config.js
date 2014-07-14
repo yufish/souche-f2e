69a388647c9c7e9dfe430087a1f375e244b551e0
xinyu198736 <xinyu198736@gmail.com>
2014-07-14 16:51:16 +0800

    deleteservice

--- tools/data-service/filters.config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    '/exa.*': {
-        get: ['checkLogin']
-    }
-}
--- tools/data-service/tools/testchild.js
+++
@@ -1,5 +0,0 @@
-var fs = require("fs")
-fs.watchFile('./temp', function(curr, prev) {
-    console.log('the current mtime is: ' + curr.mtime);
-    console.log('the previous mtime was: ' + prev.mtime);
-});
