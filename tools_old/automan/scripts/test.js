446a64c7b8052d8d82ca179e51002647d5349119
xinyu198736 <xinyu198736@gmail.com>
2014-07-23 10:49:43 +0800

    package

--- tools_old/automan/scripts/test.js
+++ tools_old/automan/scripts/test.js
@@ -1,39 +1,36 @@
-var page = require('webpage').create();
-
-// page.onInitialized = function() {
-//     page.evaluate(function() {
-//         Math.random = function() {
-//             return 42 / 100;
-//         };
-//     });
-// };
+var phantom = require('phantom');
 
 
 module.exports = {
     run: function(callback) {
         // 如果失败就callback(new Error("具体错误信息"))
         // 如果成功就callback();
-        page.open('http://www.souche.com', function(status) {
-            var result;
-            if (status !== 'success') {
-                console.log('Network error.');
-                callback(new Error("点击收藏Network error."))
-            } else {
-                page.evaluate(function() {
-                    $('.collect-box').click();
-                });
-                setTimeout(function() {
-                    var p = page.evaluate(function() {
-                        return $("#noreg-popup").css("display")
-                    });
-                    if (p == "block") {
-                        callback()
+
+        phantom.create(function(ph) {
+            ph.createPage(function(page) {
+                page.open('http://www.souche.com', function(status) {
+                    var result;
+                    if (status !== 'success') {
+                        console.log('Network error.');
+                        callback(new Error("点击收藏Network error."))
                     } else {
-                        callback(new Error("点击收藏弹窗没有显示出来"))
+                        page.evaluate(function() {
+                            $('.collect-box').click();
+                        });
+                        setTimeout(function() {
+                            var p = page.evaluate(function() {
+                                return $("#noreg-popup").css("display")
+                            });
+                            if (p == "block") {
+                                callback()
+                            } else {
+                                callback(new Error("点击收藏弹窗没有显示出来"))
+                            }
+                            ph.exit();
+                        }, 1000)
                     }
-                    phantom.exit();
-                }, 1000)
-            }
+                });
+            });
         });
 
     }
