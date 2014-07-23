9429d7496a5cf311d77214bac1fe645678301a76
xinyu198736 <xinyu198736@gmail.com>
2014-07-23 10:38:19 +0800

    自动化测试

---
+++ tools_old/automan/scripts/test.js
@@ -0,0 +1,42 @@
+var page = require('webpage').create();
+
+// page.onInitialized = function() {
+//     page.evaluate(function() {
+//         Math.random = function() {
+//             return 42 / 100;
+//         };
+//     });
+// };
+
+
+module.exports = function() {
+    return {
+        run: function(callback) {
+            // 如果失败就callback(new Error("具体错误信息"))
+            // 如果成功就callback();
+            page.open('http://www.souche.com', function(status) {
+                var result;
+                if (status !== 'success') {
+                    console.log('Network error.');
+                    callback(new Error("点击收藏Network error."))
+                } else {
+                    page.evaluate(function() {
+                        $('.collect-box').click();
+                    });
+                    setTimeout(function() {
+                        var p = page.evaluate(function() {
+                            return $("#noreg-popup").css("display")
+                        });
+                        if (p == "block") {
+                            callback()
+                        } else {
+                            callback(new Error("点击收藏弹窗没有显示出来"))
+                        }
+                        phantom.exit();
+                    }, 1000)
+                }
+            });
+
+        }
+    }
+}
