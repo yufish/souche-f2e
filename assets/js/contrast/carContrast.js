b5d0f8404798333bb7194c6876c82060ee5ac271
xinyu198736 <xinyu198736@gmail.com>
2014-07-25 18:17:57 +0800

    library

--- assets/js/contrast/carContrast.js
+++ assets/js/contrast/carContrast.js
@@ -3,18 +3,17 @@
  */
 define(function() {
     var carContrast = {};
-    var config = {
-    };
-    var ele ={
+    var config = {};
+    var ele = {
 
     };
 
-    var carCount =4;
-    var sortString=[];
-    var showScroll_x =false;
-    var contentTemplate="<th width='' class='title carname'><div style=\"width:239px\"><a><\/a><i class='close-contrast hidden'><\/i><span class='more-detail'><\/span><\/div><\/th>" +
-                          "<td class='pic'><div style=\"width:240px\"><\/div><\/td><td class='price-s'><div style=\"width:240px\"><\/div><\/td><td class='price-n'><div style=\"width:240px\"><\/div><\/td>" +
-                          "<td class='price-v'><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td>" +
+    var carCount = 4;
+    var sortString = [];
+    var showScroll_x = false;
+    var contentTemplate = "<th width='' class='title carname'><div style=\"width:239px\"><a><\/a><i class='close-contrast hidden'><\/i><span class='more-detail'><\/span><\/div><\/th>" +
+        "<td class='pic'><div style=\"width:240px\"><\/div><\/td><td class='price-s'><div style=\"width:240px\"><\/div><\/td><td class='price-n'><div style=\"width:240px\"><\/div><\/td>" +
+        "<td class='price-v'><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td>" +
         "<td width=''><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td>" +
         "<td width=''><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td>" +
         "<td width=''><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td>" +
@@ -24,8 +23,8 @@ define(function() {
         "<td width=''><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td>" +
         "<td width=''><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td><td><div style=\"width:240px\"><\/div><\/td>";
 
-    var _bind = function () {
-        $(".close-contrast").live("click", function (event) {
+    var _bind = function() {
+        $(".close-contrast").live("click", function(event) {
             var headTh = $(this).parent();
             var headThIndex = headTh.index();
 
@@ -39,22 +38,21 @@ define(function() {
                 },
                 dataType: "json",
                 context: headTh
-            }).done(function (data) {
-                if(data.result == 2) {
+            }).done(function(data) {
+                if (data.result == 2) {
                     this.deleteContent(this.parent().index());
                     delete headTh.deleteContent;
-                }
-                else {
-                }
+                } else {}
             });
 
             event.stopPropagation();
             return false;
         });
 
-        var hasTouch = false, startX, startY, cloneElement;
-        var headNavTop =$(".contrast-table").offset().top;
-        var headNavLeft =$(".contrast-table").offset().left;
+        var hasTouch = false,
+            startX, startY, cloneElement;
+        var headNavTop = $(".contrast-table").offset().top;
+        var headNavLeft = $(".contrast-table").offset().left;
         var cellWidth = $(".carname").width();
         var cellHeight = $(".carname").height();
         var moveRangeStartX = $(".carname").offset().left;
@@ -62,11 +60,11 @@ define(function() {
         var moveRangeEndX = $(".car-title").offset().left + $(".car-title").width();
         var moveRangeStartY = $(".carname").offset().top;
         var moveRangeEndY = $(".carname").offset().top + $(".carname").height();
-        var contentPixList , movePosition, defaultPosition;
+        var contentPixList, movePosition, defaultPosition;
 
         contentPixList = [];
 
-        $(".more-detail").live("mousedown", function (event) {
+        $(".more-detail").live("mousedown", function(event) {
             event.stopPropagation();
             event.preventDefault();
 
@@ -84,7 +82,7 @@ define(function() {
             });
             document.body.appendChild(cloneElement[0]);
 
-            document.body.onselectstart = document.body.ondrag = function () {
+            document.body.onselectstart = document.body.ondrag = function() {
                 return false;
             }
 
@@ -94,45 +92,41 @@ define(function() {
             }
 
             defaultPosition = $(this).parent().parent().index();
-            if(defaultPosition==0)
-            {
+            if (defaultPosition == 0) {
                 return;
             }
 
             for (var index = 0; index < contentPixList.length; index++) {
                 if ((event.pageX) < contentPixList[index] + cellWidth && (event.pageX) > contentPixList[index]) {
                     if (movePosition !== index) {
-                        movePosition  = index;
+                        movePosition = index;
                     }
                 }
             }
             return false;
         });
 
-        $(window).scroll(function (event) {
-                var current = $(document).scrollTop();
-                if (current > (headNavTop + $(".car-title").height())) {
-                    $(".contrast-table .basic-info:eq(0)").css({
-                        position: "fixed",
-                        top: "0px",
-                        backgroundColor: "white"
-                    });
-                }
-
-                else {
-                    $(".contrast-table .basic-info:eq(0)").css({
-                        position: "",
-                        top: "",
-                        left: "",
-                        backgroundColor: "white"
-                    });
-                }
-
-                $(".contrast-table .basic-info:eq(0)").css("left", headNavLeft - $(document).scrollLeft());
+        $(window).scroll(function(event) {
+            var current = $(document).scrollTop();
+            if (current > (headNavTop + $(".car-title").height())) {
+                $(".contrast-table .basic-info:eq(0)").css({
+                    position: "fixed",
+                    top: "0px",
+                    backgroundColor: "white"
+                });
+            } else {
+                $(".contrast-table .basic-info:eq(0)").css({
+                    position: "",
+                    top: "",
+                    left: "",
+                    backgroundColor: "white"
+                });
             }
-        );
 
-        $(document).mousemove(function (event) {
+            $(".contrast-table .basic-info:eq(0)").css("left", headNavLeft - $(document).scrollLeft());
+        });
+
+        $(document).mousemove(function(event) {
             if (hasTouch) {
                 y = event.pageY;
                 x = event.pageX;
@@ -144,7 +138,7 @@ define(function() {
                     });
 
                     for (var index = 0; index < contentPixList.length; index++) {
-                        if ((x-$(document).scrollLeft()) < contentPixList[index] + cellWidth && (x-$(document).scrollLeft()) > contentPixList[index]) {
+                        if ((x - $(document).scrollLeft()) < contentPixList[index] + cellWidth && (x - $(document).scrollLeft()) > contentPixList[index]) {
 
                             if (movePosition != index && (index != defaultPosition - 1) && index <= carCount) {
                                 movePosition = index;
@@ -159,7 +153,7 @@ define(function() {
             }
         });
 
-        $(document).mouseup(function () {
+        $(document).mouseup(function() {
             if (hasTouch) {
                 cloneElement.remove();
                 hasTouch = false;
@@ -171,8 +165,7 @@ define(function() {
                     var carList = $(".carname");
                     var carListLength = carList.length;
 
-                    if(defaultPosition===0)
-                    {
+                    if (defaultPosition === 0) {
                         return;
                     }
                     var moveItemList = getContentList(defaultPosition);
@@ -193,14 +186,14 @@ define(function() {
                         },
                         dataType: "json",
                         context: self
-                    }).done(function (data) {
+                    }).done(function(data) {
 
                     });
                 }
             }
         });
 
-        $(".contrast-title input").change(function () {
+        $(".contrast-title input").change(function() {
             var optimal, repeat;
             var repeat = $(".contrast-title input")[0].checked.toString();
             var optimal = $(".contrast-title input")[1].checked.toString();
@@ -208,19 +201,18 @@ define(function() {
             window.location = config.api_contrastUrl + "?repeat=" + repeat + "&optimal=" + optimal;
         });
 
-        $(".carname a").live("mouseenter", function () {
+        $(".carname a").live("mouseenter", function() {
             $(this).addClass("carNameHover");
-        }).live("mouseout", function () {
+        }).live("mouseout", function() {
             $(this).removeClass("carNameHover");
         });
     }
 
-    var getContentList =function(index)
-    {
+    var getContentList = function(index) {
         var list = $();
         var index = index;
-        $("table tr").each(function(idx,item) {
-            if($(item).find("td,th").length!=0) {
+        $("table tr").each(function(idx, item) {
+            if ($(item).find("td,th").length != 0) {
                 var deleteElement = $(item).children()[index];
                 list.push(deleteElement);
             }
@@ -259,61 +251,52 @@ define(function() {
         };
     }
 
-    var setCellContent = function(cell,content) {
+    var setCellContent = function(cell, content) {
         if (content) {
             if (content.type === "img") {
                 var img = cell.find(content.type);
-                if(img.length!=0) {
+                if (img.length != 0) {
                     cell.find(content.type).attr("src", content.content.attr("src"));
+                } else {
+                    cell.html("<img src='" + content.content.attr("src") + "' width='215px' height='1240px'>");
                 }
-                else
-                {
-                    cell.html("<img src='"+content.content.attr("src")+"' width='215px' height='1240px'>");
-                }
-            }
-            else if (content.type === "a" || content.type === "span") {
+            } else if (content.type === "a" || content.type === "span") {
                 cell.find(content.type).text(content.content);
-            }
-            else {
+            } else {
                 cell.text("");
             }
-        }
-        else {
+        } else {
             var type = getCellContent(cell).type;
             if (type === "img") {
                 cell.find(type).remove();
-            }
-            else if (type === "a" || type === "span") {
+            } else if (type === "a" || type === "span") {
                 cell.find(type).text("");
-            }
-            else {
+            } else {
                 cell.text("");
             }
         }
     }
 
-    var addNewContent=function(list,index,param) {
+    var addNewContent = function(list, index, param) {
         //console.log("insert:" + index);
         var position = index;
-        var i=0;
+        var i = 0;
         if (index != undefined) {
-            $("table tr").each(function (idx, item) {
-                if($(item).find("td,th").length!=0) {
+            $("table tr").each(function(idx, item) {
+                if ($(item).find("td,th").length != 0) {
                     if (param) {
                         $(item).find(".tempalte").remove();
                         $(list[i]).addClass("tempalte");
                         $(item.children[position]).after($(list[i]));
-                    }
-                    else {
+                    } else {
                         $(item.children[position]).after($(list[i]));
                     }
                     i++;
                 }
             });
-        }
-        else {
-            $("table tr").each(function (idx, item) {
-                if($(item).find("td,th").length!=0) {
+        } else {
+            $("table tr").each(function(idx, item) {
+                if ($(item).find("td,th").length != 0) {
                     if ($(item).find("td,th").length != 0) {
                         $(item).append($(list[i++]));
                     }
@@ -322,23 +305,21 @@ define(function() {
         }
     }
 
-    var deleteContent =function(index) {
+    var deleteContent = function(index) {
         var list = getContentList(index);
         list.remove();
         carCount--;
-        $(".table-name").width($(".basic-info").width()-17);
+        $(".table-name").width($(".basic-info").width() - 17);
     }
 
-    var init = function (_config) {
+    var init = function(_config) {
         $.extend(config, _config);
         carCount = config.carNum;
 
-        $(".table-name").width(($(".basic-info").width()-17));
+        $(".table-name").width(($(".basic-info").width() - 17));
         _bind();
     }
 
     carContrast.init = init;
     return carContrast;
 });
-
-
