709f403da24b8a433d8becf638a2f292a020f5b6
Dpangzi <qjb3013650@163.com>
2014-07-29 16:32:12 +0800

    懒加载

--- assets/js/index_v2.js
+++ assets/js/index_v2.js
@@ -77,6 +77,7 @@ define(function() {
 
 
         var getMore = function () {
+
             $("."+$("#carsNav li.active").attr("id")+" .carsMore span").html("正在获取");
 
             if ($(this).hasClass("myAdviser")) {
@@ -89,7 +90,7 @@ define(function() {
                         dataType: "json"
                     }).done(function (result) {
                         if (result.code == 204) {
-                            $("."+$("#carsNav li.active").attr("id")+" .carsMore.myAdviser").remove();
+                            $("." + $("#carsNav li.active").attr("id") + " .carsMore.myAdviser").remove();
                         }
                         else {
                             var list = result.recommendCars;
@@ -105,7 +106,6 @@ define(function() {
                                     "<\/div>" +
                                     "<div class='carTail clearfix'><span class='collect' " + (list[idx].like ? "ative" : "") + ">收藏" + list[idx].collectNum + "<\/span><span class='recommendedToday'>" + list[idx].recommendTime + "<\/span><\/div>" +
                                     "<\/div>";
-
                             }
                             $(".myAdviserContent .myAdviser").eq(0).append(template);
                             $(".myAdviserContent .myAdviser").eq(1).remove();
@@ -130,7 +130,7 @@ define(function() {
                             var template = "";
                             for (var idx = 0, len = list.length; idx < len; idx++) {
                                 template += "<div class='carsItem carItem'><a href='#' class='carImg'><img src='http://res.souche.com/" + list[idx].carPicturesVO.pictureBig + "' height='182'><\/a><a href='#' class='car-link'>" + list[idx].carVo.carOtherAllName + "<\/a>" +
-                                    "<div class='info'><span class='price'>￥$" + (list[idx].limitSpec || list[idx].price) + "万<\/span><span class='shangpai'>上牌：" + list[idx].carVo.firstLicensePlateDateShow + "<\/span><\/div>" +
+                                    "<div class='info'><span class='price'>￥" + (list[idx].limitSpec || list[idx].price) + "万<\/span><span class='shangpai'>上牌：" + list[idx].carVo.firstLicensePlateDateShow + "<\/span><\/div>" +
                                     "<div class='other'>" +
                                     "<div class='discount " + (list[idx].flashPurchase ? "" : "hidden") + "'>优惠：<span>" + (list[idx].flashPurchaseVO ? list[idx].flashPurchaseVO.totalMasterOutPrice : "") + "<\/span><\/div>" +
                                     "<div class='downCounter " + (list[idx].flashPurchase ? "" : "hidden") + "'><span>剩余时间<\/span><\/div>" +
@@ -169,6 +169,90 @@ define(function() {
         //
     }
 
+    var downCounter = function(target, fakeTime) {
+        var container = target;
+        var counter = {
+            endYear: container.attr("endYear"),
+            endMonth: container.attr("endMonth"),
+            endDay: container.attr("endDay"),
+            endHour: container.attr("endHour"),
+            serverYear: container.attr("serverYear"),
+            serverMonth: container.attr("serverMonth"),
+            serverDay: container.attr("serverDay"),
+            serverHour: container.attr("serverHour"),
+            serverMin: container.attr("serverMin"),
+            serverSec: container.attr("serverSec"),
+            offHour: 0,
+            offMin: 0,
+            offSec: 0,
+            offMSec: 0
+        };
+        var showDom = function() {
+            var zeroH = "",
+                zeroM = "",
+                zeroS = "";
+
+            if (counter.offHour < 10) {
+                zeroH = "0";
+            }
+            if (counter.offMin < 10) {
+                zeroM = "0";
+            }
+            if (counter.offSec < 10) {
+                zeroS = "0";
+            }
+
+            container.html("<span>剩余时间：<ins>" + zeroH + counter.offHour + "</ins>&nbsp时&nbsp<ins>" + zeroM + counter.offMin + "</ins>&nbsp分&nbsp<ins>" + zeroS + counter.offSec + "." + counter.offMSec + "</ins>&nbsp秒</span>");
+        };
+        var setInitTime = function() {
+            var endDate = new Date(counter.endYear, counter.endMonth, counter.endDay, counter.endHour, 0, 0);
+            var serverDate = new Date(counter.serverYear, counter.serverMonth, counter.serverDay, counter.serverHour, counter.serverMin, counter.serverSec);
+            if (fakeTime) {
+                endDate = fakeTime.endDate;
+                serverDate = fakeTime.startDate;
+            }
+            var offset = Date.parse(endDate) - Date.parse(serverDate);
+
+            if (offset < 0) {
+                counter.offMSec = 0;
+                counter.offSec = 0;
+                counter.offMin = 0;
+                counter.offHour = 0;
+                showDom();
+                return false;
+            }
+            counter.offHour = Math.floor(offset / (3600 * 1000));
+            var leave = offset % (3600 * 1000);
+            counter.offMin = Math.floor(leave / (60 * 1000));
+            var leave2 = leave % (60 * 1000);
+            counter.offSec = Math.floor(leave2 / 1000);
+            showDom();
+        };
+        setInitTime(); //初始化
+        var timer = setInterval(function() {
+            --counter.offMSec;
+            if (counter.offMSec < 0) {
+                counter.offMSec = 9;
+                --counter.offSec;
+                if (counter.offSec < 0) {
+                    counter.offSec = 59;
+                    --counter.offMin;
+                    if (counter.offMin < 0) {
+                        counter.offMin = 59;
+                        --counter.offHour;
+                        if (counter.offHour < 0) {
+                            clearInterval(timer);
+                            counter.offSec = 0;
+                            counter.offMin = 0;
+                            counter.offHour = 0;
+                        }
+                    }
+                }
+            }
+            showDom();
+        }, 100);
+    };
+
     ////
     return {
         init: function(_config) {
@@ -176,8 +260,8 @@ define(function() {
 
             _bind();
 
-            require(["index/qiugou_v2", "index/qiugouModel", 'souche/custom-select', "index/modelSeries", "souche/down-counter","index/collect","lib/lazyload"],
-                function (qiugou, qiugouModel, customSelect, modelSeries, downCounter,collect,lazyload) {
+            require(["index/qiugou_v2", "index/qiugouModel", 'souche/custom-select', "index/modelSeries","index/collect","lib/lazyload"],
+                function (qiugou, qiugouModel, customSelect, modelSeries,collect,lazyload) {
 
                     qiugou.init(config);
                     //modelSeriesModel.init(config);
@@ -207,10 +291,13 @@ define(function() {
                         downCounter($this);
                     });
 
+
+
+
+
                     collect.init(config);
 
                     $(".carsContent img").lazyload({
-                        effect: "fadeIn"
                     });
 
 
