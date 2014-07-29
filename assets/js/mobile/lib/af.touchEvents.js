9d3427ff7cb4819da5a7cf424167fd0497446004
jzlxiaohei <jzlxiaohei@163.com>
2014-07-29 21:21:00 +0800

    Update af.touchEvents.js

--- assets/js/mobile/lib/af.touchEvents.js
+++ assets/js/mobile/lib/af.touchEvents.js
@@ -84,7 +84,7 @@
                 touch.el.trigger("doubleTap");
                 touch = {};
             } else if (touch.x2 > 0 || touch.y2 > 0) {
-                (Math.abs(touch.x1 - touch.x2) > 60 || Math.abs(touch.y1 - touch.y2) > 60) &&
+                (Math.abs(touch.x1 - touch.x2) > 30 || Math.abs(touch.y1 - touch.y2) > 30) &&
                 touch.el.trigger("swipe") &&
                 touch.el.trigger("swipe" + (swipeDirection(touch.x1, touch.x2, touch.y1, touch.y2)), touch);
                 touch.x1 = touch.x2 = touch.y1 = touch.y2 = touch.last = 0;
