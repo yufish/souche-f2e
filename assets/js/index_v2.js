2717b1fd3fd6bc97282b619a90a04f2dc6484b28
xinyu198736 <xinyu198736@gmail.com>
2014-07-29 15:43:45 +0800

    车神动画效果

--- assets/js/index_v2.js
+++ assets/js/index_v2.js
@@ -1,18 +1,19 @@
-define(function() {
+define(['index/car-god'], function(carGod) {
     var config = {};
-    var myAdviserPageIndex=1 , hotNewCarsPageIndex=1;
+    var myAdviserPageIndex = 1,
+        hotNewCarsPageIndex = 1;
 
     var _bind = function() {
         var timeout = null;
 
-        $(".car-brand,.car-models,.car-price").mouseenter(function (event) {
+        $(".car-brand,.car-models,.car-price").mouseenter(function(event) {
 
             event.stopPropagation();
             var self = this;
             window.clearTimeout(timeout);
 
             $(".models-inner,.brand-inner,.price-inner").hide();
-            timeout = window.setTimeout(function () {
+            timeout = window.setTimeout(function() {
                 if (eval("''+/*@cc_on" + " @_jscript_version@*/-0") * 1 == 5.7) {
                     if ($(self).hasClass("car-models")) {
                         $(".car-price").css("visibility", "hidden");
@@ -28,7 +29,7 @@ define(function() {
             return false;
         });
 
-        $(".car-brand,.car-models,.car-price").mouseleave(function () {
+        $(".car-brand,.car-models,.car-price").mouseleave(function() {
 
             var self = this;
             window.clearTimeout(timeout);
@@ -45,7 +46,7 @@ define(function() {
             }
         });
 
-        $(".carItem").mouseenter(function (event) {
+        $(".carItem").mouseenter(function(event) {
             event.stopPropagation();
             var self = $(this);
 
@@ -56,11 +57,10 @@ define(function() {
                 height: 194 + "px",
                 top: "-3px",
                 left: "-3px"
-            }, 250, function () {
-            });
+            }, 250, function() {});
         });
 
-        $(".carItem").mouseleave(function (event) {
+        $(".carItem").mouseleave(function(event) {
             event.stopPropagation();
             var self = $(this);
 
@@ -70,86 +70,85 @@ define(function() {
                 height: 182 + "px",
                 top: "0px",
                 left: "0px"
-            }, 250, function () {
-            });
+            }, 250, function() {});
         });
 
         //查看更多
-        $(".carsMore").click(function () {
+        $(".carsMore").click(function() {
             $("#carsMore span").html("正在获取");
 
             if ($(this).hasClass("myAdviser")) {
                 myAdviserPageIndex;
                 var url = config.getMoreUserRecommend_api + "=" + myAdviserPageIndex;
-                $.ajax(
-                    {
-                        url: url,
-                        type: "GET",
-                        dataType: "json"
-                    }).done(function (result) {
-                        if (result.code == 204) {
+                $.ajax({
+                    url: url,
+                    type: "GET",
+                    dataType: "json"
+                }).done(function(result) {
+                    if (result.code == 204) {
+                        $(".carsMore.myAdviser").remove();
+                    } else {
+                        var list = result.recommendCars;
+                        if (!result.hasNext) {
                             $(".carsMore.myAdviser").remove();
                         }
-                        else {
-                            var list = result.recommendCars;
-                            if (!result.hasNext) {
-                                $(".carsMore.myAdviser").remove();
-                            }
-                            var template = "";
-                            for (var idx = 0, len = list.length; idx < len; idx++) {
-                                template += "<div class=\"carsItem carItem\"><a href=\"#\" class=\"carImg\"><img src='http://res.souche.com/" + list[idx].carPicturesVO.pictureBig + "' height='182'><\/a><a href='#' class='car-link'>" + list[idx].carVo.carOtherAllName + "<\/a>" +
-                                    "<div class='info'><span class='price'>￥" + list[idx].carVo.salePriceToString + "万<\/span><span class='shangpai'>上牌：" + list[idx].carVo.firstLicensePlateDateShow + "<\/span><\/div>" +
-                                    "<div class='other'>" +
-                                    "<div title='" + list[idx].recommendReasonStr + "' class='recommended'><span class='" + (list[idx].recommendReasonStr ? "" : "hidden") + "' >推荐理由：" + list[idx].recommendReasonStr + "<\/span><\/div>" +
-                                    "<\/div>" +
-                                    "<div class='carTail clearfix'><span class='collect' " + (list[idx].like ? "ative" : "") + ">收藏" + list[idx].collectNum + "<\/span><span class='recommendedToday'>" + list[idx].recommendTime + "<\/span><\/div>" +
-                                    "<\/div>";
+                        var template = "";
+                        for (var idx = 0, len = list.length; idx < len; idx++) {
+                            template += "<div class=\"carsItem carItem\"><a href=\"#\" class=\"carImg\"><img src='http://res.souche.com/" + list[idx].carPicturesVO.pictureBig + "' height='182'><\/a><a href='#' class='car-link'>" + list[idx].carVo.carOtherAllName + "<\/a>" +
+                                "<div class='info'><span class='price'>￥" + list[idx].carVo.salePriceToString + "万<\/span><span class='shangpai'>上牌：" + list[idx].carVo.firstLicensePlateDateShow + "<\/span><\/div>" +
+                                "<div class='other'>" +
+                                "<div title='" + list[idx].recommendReasonStr + "' class='recommended'><span class='" + (list[idx].recommendReasonStr ? "" : "hidden") + "' >推荐理由：" + list[idx].recommendReasonStr + "<\/span><\/div>" +
+                                "<\/div>" +
+                                "<div class='carTail clearfix'><span class='collect' " + (list[idx].like ? "ative" : "") + ">收藏" + list[idx].collectNum + "<\/span><span class='recommendedToday'>" + list[idx].recommendTime + "<\/span><\/div>" +
+                                "<\/div>";
 
-                            }
-                            $("#carsContent .myAdviser").append(template);
-                            $("#carsMore span").html("查看更多");
                         }
-                    });
-            }
-            else {
+                        $("#carsContent .myAdviser").append(template);
+                        $("#carsMore span").html("查看更多");
+                    }
+                });
+            } else {
                 var url = config.getMoreHotCars_api + hotNewCarsPageIndex;
                 hotNewCarsPageIndex++;
-                $.ajax(
-                    {
-                        url: url,
-                        type: "GET",
-                        dataType: "json"
-                    }).done(function (result) {
-                        if (result.code == 204) {
-                            $(".carsMore.hotNewCars").remove();
-                        }
-                        else {
-                            var list = result.newCars.items;
+                $.ajax({
+                    url: url,
+                    type: "GET",
+                    dataType: "json"
+                }).done(function(result) {
+                    if (result.code == 204) {
+                        $(".carsMore.hotNewCars").remove();
+                    } else {
+                        var list = result.newCars.items;
 
-                            var template = "";
-                            for (var idx = 0, len = list.length; idx < len; idx++) {
-                                template += "<div class='carsItem carItem'><a href='#' class='carImg'><img src='http://res.souche.com/" + list[idx].carPicturesVO.pictureBig + "' height='182'><\/a><a href='#' class='car-link'>" + list[idx].carVo.carOtherAllName + "<\/a>" +
-                                    "<div class='info'><span class='price'>￥$" + (list[idx].limitSpec || list[idx].price) + "万<\/span><span class='shangpai'>上牌：" + list[idx].carVo.firstLicensePlateDateShow + "<\/span><\/div>" +
-                                    "<div class='other'>" +
-                                    "<div class='discount " + (list[idx].flashPurchase ? "" : "hidden") + "'>优惠：<span>" + (list[idx].flashPurchaseVO ? list[idx].flashPurchaseVO.totalMasterOutPrice : "") + "<\/span><\/div>" +
-                                    "<div class='downCounter " + (list[idx].flashPurchase ? "" : "hidden") + "'><span>剩余时间<\/span><\/div>" +
-                                    "<\/div>" +
-                                    "<div class='carTail'><span class='collect ative'>收藏 " + list[idx].carFavoriteNum + "<\/span><span class='recommendedToday'>今日推荐<\/span><\/div>" +
-                                    "<\/div>";
+                        var template = "";
+                        for (var idx = 0, len = list.length; idx < len; idx++) {
+                            template += "<div class='carsItem carItem'><a href='#' class='carImg'><img src='http://res.souche.com/" + list[idx].carPicturesVO.pictureBig + "' height='182'><\/a><a href='#' class='car-link'>" + list[idx].carVo.carOtherAllName + "<\/a>" +
+                                "<div class='info'><span class='price'>￥$" + (list[idx].limitSpec || list[idx].price) + "万<\/span><span class='shangpai'>上牌：" + list[idx].carVo.firstLicensePlateDateShow + "<\/span><\/div>" +
+                                "<div class='other'>" +
+                                "<div class='discount " + (list[idx].flashPurchase ? "" : "hidden") + "'>优惠：<span>" + (list[idx].flashPurchaseVO ? list[idx].flashPurchaseVO.totalMasterOutPrice : "") + "<\/span><\/div>" +
+                                "<div class='downCounter " + (list[idx].flashPurchase ? "" : "hidden") + "'><span>剩余时间<\/span><\/div>" +
+                                "<\/div>" +
+                                "<div class='carTail'><span class='collect ative'>收藏 " + list[idx].carFavoriteNum + "<\/span><span class='recommendedToday'>今日推荐<\/span><\/div>" +
+                                "<\/div>";
 
-                            }
-                            $("#carsContent .hotNewCars").append(template);
                         }
-                    });
+                        $("#carsContent .hotNewCars").append(template);
+                    }
+                });
             }
         });
         //
 
         //head 搜索框
-        require(["souche/realTimeDown"], function (down) {
-            down.init($(".search"), {url: config.search_api, type: "GET",dataType:"json", success: function () {
-                alert(1);
-            }}, 900);
+        require(["souche/realTimeDown"], function(down) {
+            down.init($(".search"), {
+                url: config.search_api,
+                type: "GET",
+                dataType: "json",
+                success: function() {
+                    alert(1);
+                }
+            }, 900);
 
         });
         //
@@ -161,9 +160,9 @@ define(function() {
             $.extend(config, _config);
 
             _bind();
-
+            carGod.init();
             require(["index/qiugou_v2", "index/qiugouModel", 'souche/custom-select', "index/modelSeries", "souche/down-counter"],
-                function (qiugou, qiugouModel, customSelect, modelSeries, downCounter) {
+                function(qiugou, qiugouModel, customSelect, modelSeries, downCounter) {
 
                     qiugou.init(config);
                     //modelSeriesModel.init(config);
@@ -188,7 +187,7 @@ define(function() {
                         multi: false
                     });
 
-                    $(".down-counter").each(function () {
+                    $(".down-counter").each(function() {
                         var $this = $(this);
                         downCounter($this);
                     });
