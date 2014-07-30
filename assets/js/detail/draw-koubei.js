d82dc934856dfee6da11d7d43f8b77b566803d31
xinyu198736 <xinyu198736@gmail.com>
2014-07-30 16:14:02 +0800

    产品库

--- assets/js/detail/draw-koubei.js
+++ assets/js/detail/draw-koubei.js
@@ -36,7 +36,6 @@ define(['lib/mustache', 'lib/svg.min', 'lib/queuedo'], function(Mustache, SVG, q
     var DrawKoubei = {
         draw: function(_data) {
             data = _data;
-            var self = this;
             var totalScore = 0;
             for (var i = 0; i < data.items.length; i++) {
                 rateData.push(data.items[i].rate);
@@ -44,14 +43,9 @@ define(['lib/mustache', 'lib/svg.min', 'lib/queuedo'], function(Mustache, SVG, q
             }
 
             item_tpl = $("#koubei_item_template").html();
+            this.drawRadar();
+            this.drawLabel();
 
-
-            $(window).on("nav_change", function(e, id) {
-                if (id == "onsale_koubei") {
-                    self.drawRadar();
-                    self.drawLabel();
-                }
-            })
             $(".all-score em").html((totalScore / 9).toFixed(2))
             $(".series-tab").html("")
             var tab = $("<div class='series-tab-item item-active'></div>").html(data.seriesName).attr("data-code", data.seriesCode);
