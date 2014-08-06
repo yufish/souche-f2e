e8c7a5331347cd149da7bdf2dc09c20410aeab06
xinyu198736 <xinyu198736@gmail.com>
2014-08-06 11:08:20 +0800

    文件清理

--- tools/data-service/models/F2EOffline.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-
-    url:String,
-    pv:Number,
-    uv:Number,
-    click_count:Number,
-    click_type_data:String
-};
--- tools/data-service/server.js
+++
@@ -1,5 +0,0 @@
-//引入express服务器
-var server = require('./index.js')
-require('http').createServer(server).listen(server.get('port'), function() {
-    console.log('Express server listening on port ' + server.get('port'));
-});
