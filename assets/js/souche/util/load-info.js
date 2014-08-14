2ea3ccd15eab6cbc05ad5366f29400a1ace73441
xinyu198736 <xinyu198736@gmail.com>
2014-08-14 16:23:25 +0800

    util

--- assets/js/souche/util/load-info.js
+++ assets/js/souche/util/load-info.js
@@ -6,6 +6,8 @@
 define(['souche/util/sc-db'], function(DB) {
     var BRAND_KEY = "BRAND_CACHE_KEY";
     var SERIES_KEY = "SERIES_CACHE_KEY";
+    var PROVINCE_KEY = "PROVINCE_CACHE_KEY"
+    var CITY_KEY = "CITY_CACHE_KEY"
     var db = new DB("LOADINFO")
     return {
         loadBrands: function(callback) {
@@ -65,6 +67,66 @@ define(['souche/util/sc-db'], function(DB) {
                     },
                     failure: function() {
                         // alert("车系信息请求出错，刷新后再试")
+                    }
+                });
+            }
+        },
+        loadProvince: function(callback) {
+            var data = null;
+            try {
+                data = db.get(PROVINCE_KEY);
+            } catch (e) {
+
+            }
+            if (data) {
+                callback(data);
+            } else {
+                $.ajax({
+                    url: contextPath + "/pages/dicAction/loadRootLevel.json?type=area",
+                    dataType: "json",
+                    data: {
+                        type: "area"
+                    },
+                    success: function(data) {
+                        db.set(PROVINCE_KEY, data);
+                        callback(data);
+
+                    },
+                    error: function() {
+                        // alert("品牌信息请求出错，刷新后再试")
+                    },
+                    failure: function() {
+                        // alert("品牌信息请求出错，刷新后再试")
+                    }
+                });
+            }
+        },
+        loadCity: function(provinceCode, callback) {
+            var data = null;
+            try {
+                data = db.get(CITY_KEY);
+            } catch (e) {
+
+            }
+            if (data) {
+                callback(data);
+            } else {
+                $.ajax({
+                    url: contextPath + "/pages/dicAction/loadRootLevel.json?request_message={\"code\":\"" + provinceCode + "\",\"type\":\"area\"}",
+                    dataType: "json",
+                    data: {
+
+                    },
+                    success: function(data) {
+                        db.set(CITY_KEY, data);
+                        callback(data);
+
+                    },
+                    error: function() {
+                        // alert("品牌信息请求出错，刷新后再试")
+                    },
+                    failure: function() {
+                        // alert("品牌信息请求出错，刷新后再试")
                     }
                 });
             }
