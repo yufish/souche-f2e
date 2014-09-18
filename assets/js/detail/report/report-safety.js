27805a29f5e4a2067a43206d213fcfed41f7646e
teamrun <chenllos@163.com>
2014-09-18 15:43:30 +0800

    柱状图投入使用, 添加getConfig, 动画等要素

--- assets/js/detail/report/report-safety.js
+++ assets/js/detail/report/report-safety.js
@@ -1,103 +1,182 @@
 define(['detail/report/histogram'], function(Histogram){
 
-
-
-
-    var demoConfig = {
-        y:{
+    var baseConfig = {
+        y: {
             show: true,
-            style: {
-                width: '58px',
-                dis: '20px'
-            },
-
-            axix: {
-                style: {},
-                className: 'y-axis'
-            },
-            scale: {
-                style: {},
-                className: 'y-scale-item',
-                items: [
-                    {text: '1.9mm', value: 1.9},
-                    {text: '1.6mm<br/>警戒值', value: 1.6},
-                    {text: '1.2mm', value: 1.2}
-                ],
-                valueStyleProp: 'top'
-            }
+            style: { width: '58px', dis: '20px'},
+            axis: { className: 'y-axis' },
+            scale: { className: 'y-scale-item', items: [], valueStyleProp: 'top' }
+        },
+        x: {
+            show: true,
+            axis: { className: 'x-axis' },
+            scale: { className: 'x-scale-item', items: [] }
         },
         bar: {
-            style: {
-                width: '50px',
-                height: '215px',
-                dis: '25px'
-            },
-            items: [
-                {
-                    // join name and value together and show as title
-                    name: '左前轮',
-                    value: 1.8
-                },
-                {
-                    name: '左后轮',
-                    value: 1.55
-                },
-                {
-                    name: '右前轮',
-                    value: 1.65
-                },
-                {
-                    name: '右后轮',
-                    value: 1.75
-                }
-            ],
+            style: { width: '50px', height: '215px', dis: '25px' },
+            items: [],
             max: 1.9,
             min: 1.2,
             guide: 1.6,
             cond: {
-                more: {
-                    className: 'normal',
-                    text: '安全'
-                },
-                less: {
-                    className: 'danger',
-                    text: '危险'
-                }
-            }
-            // another version:
-            // if guide is a array
-            // classes can have a between porp
-            // guide: [1.5, 1.7],
-            // classes: {
-            //     more: 'danger',
-            //     less: 'danger',
-            //     between: 'normal'
-            // }
-        },
-        x: {
-            show: true,
-            axix: {
-                className: 'x-axis'
+                more: { className: 'normal', text: '安全' },
+                less: { className: 'danger', text: '危险' }
             },
-            scale: {
-                className: 'x-scale-item',
-                items: [
-                    {text: '左前轮'},
-                    {text: '左后轮'},
-                    {text: '右前轮'},
-                    {text: '右后轮'}
-                ]
-            }
-        },
-        ctn: $('.wheel-depth .safety-item-bd')
+            // 初始时 不通过值判断class
+            condClass: false
+        }
     };
 
+
+    function getConfig( x, y, bar, ctn){
+        var newConf = $.extend(true, {}, baseConfig);
+        if(x){
+            newConf.x.scale.items = x;
+        }
+        else{
+            newConf.x.show = false;
+        }
+        if(y){
+            newConf.y.scale.items = y;
+        }
+        else{
+            newConf.y.show = false;
+        }
+        newConf.bar.items = bar;
+        newConf.ctn = ctn;
+        // console.log(newConf);
+        return newConf;
+    }
+
+    var wheelDepth = {
+        init: function(){
+            var hist = new Histogram(wheelDepth.getConfig());
+            wheelDepth.hist = hist;
+            wheelDepth.bind();
+            // for debug
+            window.hist = hist;
+        },
+        getConfig: function(){
+            var yItems =  [
+                    {text: '1.9mm', value: 1.9},
+                    {text: '1.6mm<br/>警戒值', value: 1.6},
+                    {text: '1.2mm', value: 1.2}
+                    ];
+            var xItems =  [{text: '左前轮'},
+                    {text: '左后轮'},
+                    {text: '右前轮'},
+                    {text: '右后轮'} ];
+            var barItems = [
+                { name: '左前轮', value:  0},
+                { name: '左后轮', value:  0},
+                { name: '右前轮', value:  0},
+                { name: '右后轮', value:  0}
+            ];
+            return getConfig( xItems, yItems, barItems, $('.wheel-depth .safety-item-bd') );
+        },
+        bind: function(){
+            setTimeout(function(){
+                wheelDepth.hist.updateBar([1.8, 1.55, 1.65, 1.75]);
+            }, 3000);
+        }
+    };
+
+    var brakeThickness = {
+        init: function(){
+            var hist = new Histogram(brakeThickness.getConfig());
+            brakeThickness.hist = hist;
+            brakeThickness.bind();
+        },
+        getConfig: function(){
+            var yItems =  [
+                    {text: '1.9mm', value: 1.9},
+                    {text: '1.6mm<br/>警戒值', value: 1.6},
+                    {text: '1.2mm', value: 1.2}
+                    ];
+            var xItems =  [{text: '左前轮'},
+                    {text: '左后轮'},
+                    {text: '右前轮'},
+                    {text: '右后轮'} ];
+            var barItems = [
+                { name: '左前轮', value:  0},
+                { name: '左后轮', value:  0},
+                { name: '右前轮', value:  0},
+                { name: '右后轮', value:  0}
+            ];
+            return getConfig( xItems, yItems, barItems, $('.brake-thickness .safety-item-bd') );
+        },
+        bind: function(){
+            setTimeout(function(){
+                brakeThickness.hist.updateBar([1.65, 1.85, 1.65, 1.7]);
+            }, 3000);
+        }
+    };
+
+    var liquidLevel = {
+        init: function(){
+            var hist = new Histogram(liquidLevel.getConfig());
+            liquidLevel.hist = hist;
+            liquidLevel.bind();
+        },
+        getConfig: function(){
+            var yItems =  [
+                    {text: '1.9mm', value: 1.9},
+                    {text: '1.6mm<br/>警戒值', value: 1.6},
+                    {text: '1.2mm', value: 1.2}
+                    ];
+            var xItems =  [{text: '制动液'},
+                    {text: '机油'},
+                    {text: '助力油'},
+                    {text: '变速箱'} ];
+            var barItems = [
+                { name: '左前轮', value:  0},
+                { name: '左后轮', value:  0},
+                { name: '右前轮', value:  0},
+                { name: '右后轮', value:  0}
+            ];
+            return getConfig( xItems, yItems, barItems, $('.liquid-level .safety-item-bd') );
+        },
+        bind: function(){
+            setTimeout(function(){
+                liquidLevel.hist.updateBar([1.65, 1.85, 1.65, 1.7]);
+            }, 3000);
+        }
+    };
+
+    var antiFreeze = {
+        init: function(){
+            var hist = new Histogram(antiFreeze.getConfig());
+            antiFreeze.hist = hist;
+            antiFreeze.bind();
+        },
+        getConfig: function(){
+            var yItems =  [
+                    {text: '-25C<br/>最高警戒值', value: -25},
+                    {text: '-45C<br/>最低警戒值', value: -45}
+                    ];
+            var xItems =  false;
+            var barItems = [
+                { name: '防冻液冰点', value:  0}
+            ];
+            return getConfig( xItems, yItems, barItems, $('.antifreeze .safety-item-bd') );
+        },
+        bind: function(){
+            setTimeout(function(){
+                antiFreeze.hist.updateBar([30]);
+            }, 6000);
+        }
+    };
     
+
+
 
 
     var safetyModule = {
         init: function(){
-            var hist = new Histogram(demoConfig);
+            wheelDepth.init();
+            brakeThickness.init();
+            liquidLevel.init();
+            // antiFreeze.init();
         }
     };
     
