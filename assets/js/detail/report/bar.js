54314f1a35b39c97f0160fb6113360206454d606
teamrun <chenllos@163.com>
2014-09-18 13:41:34 +0800

    完成柱状图的模块化, 添加质检报告js入口

--- assets/js/detail/report/bar.js
+++ assets/js/detail/report/bar.js
@@ -1,4 +1,4 @@
-(function(){
+define(function(){
 
     // config
     //      height
@@ -15,8 +15,6 @@
     //              
     //          less: ...
     //          between: ...
-    // 
-    //          
 
     /* demo: 
     var demoConfig = {
@@ -36,16 +34,21 @@
         }
     };
     */
+    // <li class="" style="" title="">
+    //     <div class="bar-value " style="">
+    //         <div class="value-text"></div>
+    //     </div>
+    //     <div class="guideLine"></div>
+    // </li>
 
 
     var Bar = function(config){
         var li = document.createElement('li');
         var classes = ['chart-bar'];
+        this.name = config.name;
+        this.value = config.value;
         
-        var html = '';
-        // var styleStr = 
-        html += buildBarValue(config);
-        html += buildGuideLine(config);
+        
 
         var styleStr = '';
         styleStr += 'width:' + config.style.width + ';';
@@ -53,51 +56,101 @@
 
         li.setAttribute('class', classes.join(' '));
         li.setAttribute('style', styleStr);
-        li.innerHTML = html;
-        return li;
-    }
+        this.el = li;
 
-    function buildBarValue(config){
+        // var html = '';
+        // // var styleStr = 
+        // html += buildBarValue(config);
+        // html += buildGuideLine(config);
+        // li.innerHTML = html;
+        var barValue = document.createElement('div');
+        this.barValue = barValue;
+        var barValleDetail = getBarValueDetail(config);
+        for( var i in barValleDetail.attr){
+            this.barValue.setAttribute( i.substr(1), barValleDetail.attr[i] );
+        }
+        this.barValue.innerHTML = '<div class="value-text">' + barValleDetail.content + '</div>';
+        this.el.appendChild(this.barValue);
+
+        var guideStyleArr = getGuideLineStyle(config);
+        this.guideLineArr = [];
+        for( var i=0, j=guideStyleArr.length; i<j; i++ ){
+            var el = document.createElement('div');
+            el.setAttribute('class', 'guideLine');
+            this.guideLineArr.push(el);
+            this.guideLineArr[i].style = guideStyleArr[i];
+
+            this.el.appendChild( this.guideLineArr[i] );
+        }
+
+        return this;
+    }
+    Bar.prototype.update = function(config) {
+        // 更新bar
+        var barValleDetail = getBarValueDetail(config);
+        for( var i in barValleDetail.attr){
+            this.barValue.setAttribute( i.substr(1), barValleDetail.attr[i] );
+        }
+        this.barValue.innerHTML = '<div class="value-text">' + barValleDetail.content + '</div>';
+        // this.el.appendChild(this.barValue);
+
+        // 更新guideline
+        var guideStyleArr = getGuideLineStyle(config);
+        for( var i=0, j=guideStyleArr.length; i<j; i++ ){
+            var el = document.createElement('div');
+            this.guideLineArr[i].style = guideStyleArr[i];
+        }
+    };
+
+    function getBarValueDetail(config){
         var value = config.value;
         var max = config.max;
         var min = config.min;
         var guide = config.guide;
         var cond = config.cond;
-        // var classes
-        var html = '';
+        
+        var result = {};
+
         var valueStyle = 'height: '+ safeValue((value - min)/(max - min))*100 +'%;';
         var classArr = ['bar-value'];
         var text = '';
         if( typeof guide === 'number'){
             if(value >= guide){
                 classArr.push(cond.more.className);
-                text = cond.more.text;
+                text = cond.more.text || '';
             }
             else if(value < guide){
                 classArr.push(cond.less.className);
-                text = cond.less.text;
+                text = cond.less.text || '';
             }
         }
         // else{
 
         // }
 
-        html += '<div class="'+classArr.join(' ')+'" style="'+valueStyle+'">';
-        if(text){
-            html += '<div class="value-text">'+text+'</div>';
-        }
-        html += '</div>';
-        return html;
+        result.attr = {
+            _style: valueStyle,
+            _class: classArr.join(' ')
+        };
+        result.content = text;
+        return result;
     }
-    function buildGuideLine( config ){
+    function getGuideLineStyle( config ){
         var max = config.max;
         var min = config.min;
         var guide = config.guide;
-        var html = '';
-        var guideLineStyle = 'top: '+ safeValue((max - guide)/(max - min))*100 +'%'
-        html += '<div class="guideLine">';
-        html += '</div>';
-        return html;
+        // var html = '';
+        if( typeof guide == 'number' ){
+            return 'top: '+ safeValue((max - guide)/(max - min))*100 +'%;'
+        }
+        else if( guide.isArray() ){
+            var arr = [];
+            for( var i=0, j=guide.length; i<j; i++ ){
+                arr.push( 'top: '+ safeValue((max - guide)/(max - min))*100 +'%;' );
+            }
+            return arr;
+        }
+        return false;
     }
     function safeValue(val, safeZone){
         safeZone = safeZone || {max: 1, min:0};
@@ -112,5 +165,7 @@
         }
     }
 
-    window.Bar = Bar;
-})();
+
+
+    return Bar;
+});
