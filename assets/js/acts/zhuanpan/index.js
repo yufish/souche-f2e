022dd52964bea92842aff33ea4b42fc8dd85543d
pe_Jagten <p19911227@163.com>
2014-09-22 15:34:11 +0800

    zhuanpan

--- assets/js/acts/zhuanpan/index.js
+++ assets/js/acts/zhuanpan/index.js
@@ -1,11 +1,11 @@
-define(['acts/zhuanpan/zhuanpan'], function(zhuanpan){
+define(['acts/zhuanpan/zhuanpan'], function(zhuanpan) {
     var _config = null;
     var phoneReg = /^1[34578][0-9]{9}$/;
-    var LTE_IE9_REG =  /MSIE\ [6789].0/i;
+    var LTE_IE9_REG = /MSIE\ [6789].0/i;
 
     // 版本 >= 9 的IE  不支持transition, 用flash代替
     var isLowIE = LTE_IE9_REG.test(navigator.userAgent);
-    if(isLowIE){
+    if (isLowIE) {
         $('.zhuanpan-popup .close').addClass('hidden');
     }
     // 开始转: 显示flash ele
@@ -13,90 +13,86 @@ define(['acts/zhuanpan/zhuanpan'], function(zhuanpan){
 
 
     var _view = {
-        readyPop: function(){
+        readyPop: function() {
             $('.mask').removeClass('hidden');
             $('.zhuanpan-popup').removeClass('hidden');
         },
-        readyHide: function(){
+        readyHide: function() {
             $('.mask').addClass('hidden');
             $('.zhuanpan-popup').addClass('hidden');
         }
 
     };
     var _data = {
-        getPrize: function(cb){
+        getPrize: function(cb) {
             $.getJSON(_config.getPrize, cb);
         }
     };
     var prizeGot = false;
 
     var _event = {
-        bind: function(){
+        bind: function() {
             $('#start-turn').on('click', _event.startHandler);
             $('.zhuanpan-popup .close').on('click', _event.closePop);
             $('#zhuanpan-form').on('submit', _event.checkPhone);
         },
-        closePop: function(){
+        closePop: function() {
             _view.readyHide();
             $('.wrong').addClass('hidden');
             $('.zhongjiang').addClass('hidden');
             $('.choujiang').addClass('hidden');
             $('#flash-ctn').addClass('hidden');
         },
-        startHandler: function(){
+        startHandler: function() {
             _view.readyPop();
-            if( prizeGot ){
+            if (prizeGot) {
                 $('.wrong').removeClass('hidden');
                 return;
             }
             $('.choujiang').removeClass('hidden');
         },
-        checkPhone: function(e){
+        checkPhone: function(e) {
             e.preventDefault();
             var phone = $.trim($('#zhuanpan-phone').val());
-            if(phoneReg.test(phone)){
-                Souche.PhoneRegister( phone, _event.goGet);
-            }
-            else{
+            if (phoneReg.test(phone)) {
+                Souche.PhoneRegister(phone, _event.goGet);
+            } else {
                 alert('请填写正确的手机号码');
             }
             return;
         },
-        goGet: function(){
+        goGet: function() {
             prizeGot = true;
             _event.closePop();
-            if(isLowIE){
+            if (isLowIE) {
                 // 显示flash
                 $('#flash-ctn').removeClass('hidden');
-            }
-            else{
+            } else {
                 zhuanpan.reset();
             }
-            
-            _data.getPrize(function(data,status){
 
-                if( status === 'success' ){
+            _data.getPrize(function(data, status) {
+
+                if (status === 'success') {
                     // test & dev
                     // data.code = 200;
                     // data.prize = zhuanpan.randomPrize();
 
-                    if(data.code == 401){
+                    if (data.code == 401) {
                         // 提示重复抽奖
-                         $('#flash-ctn').addClass('hidden');
-                         _view.readyPop();
+                        $('#flash-ctn').addClass('hidden');
+                        _view.readyPop();
                         $('.wrong').removeClass('hidden');
-                    }
-                    else if(data.prize > 0 && data.prize <=5 ){
+                    } else if (data.prize > 0 && data.prize <= 5) {
                         var prizeText = '';
                         $('#prize-text').text(zhuanConfig.angles[data.prize].name);
                         var time = 0;
-                        if( isLowIE ){
+                        if (isLowIE) {
                             time = 1500;
-                        }
-                        else{
+                        } else {
                             time = zhuanpan.startZhuan(data.prize);
                         }
-                        setTimeout(function(){
+                        setTimeout(function() {
                             $('#flash-ctn').addClass('hidden');
                             _view.readyPop();
                             $('.zhongjiang').removeClass('hidden');
@@ -146,7 +142,8 @@ define(['acts/zhuanpan/zhuanpan'], function(zhuanpan){
             }
         }
     };
-    function init(config){
+
+    function init(config) {
         _config = config;
         zhuanpan.init(zhuanConfig);
 
