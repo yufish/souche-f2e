dfc9479996acf9c39994e307c96d0f82f3f49c39
xinyu198736 <xinyu198736@gmail.com>
2014-09-28 11:06:32 +0800

    首页卡片

--- assets/js/wannaBuy/carOperate.js
+++ assets/js/wannaBuy/carOperate.js
@@ -70,83 +70,85 @@ define(function(){
                 $(this).find("input")[0].checked=!$(this).find("input")[0].checked;
             }
             self.e = e;
-            
-            if(!$(this).find("input")[0].checked) {
-                if(!!carid) {
-                    $.ajax({
-                        type : "POST",
-                        url : config.addContrast,
-                        data: {
-                            carId: carid
-                        },
-                        dataType : "json",
-                        context : self
-                    }).done(function(data) {
-                        if(data.result == 2) {
-                            self.find("input").attr("checked", 'true');
+            Souche.MiniLogin.checkLogin(function(){
+                if(!$(this).find("input")[0].checked) {
+                    if(!!carid) {
+                        $.ajax({
+                            type : "POST",
+                            url : config.addContrast,
+                            data: {
+                                carId: carid
+                            },
+                            dataType : "json",
+                            context : self
+                        }).done(function(data) {
+                            if(data.result == 2) {
+                                self.find("input").attr("checked", 'true');
 
-                            var cloneElement = self.clone();
-                            cloneElement.css({
-                                opacity : 0.8,
-                                position : 'absolute',
-                                top : this.e.pageY + 'px',
-                                left : this.e.pageX + 'px',
-                                backgroundColor : "#BCEE68"
-                            });
+                                var cloneElement = self.clone();
+                                cloneElement.css({
+                                    opacity : 0.8,
+                                    position : 'absolute',
+                                    top : this.e.pageY + 'px',
+                                    left : this.e.pageX + 'px',
+                                    backgroundColor : "#BCEE68"
+                                });
 
-                            var endX = $(".side-box .contrast-img").offset().left;
-                            var endY = $(".side-box .contrast-img").offset().top;
+                                var endX = $(".side-box .contrast-img").offset().left;
+                                var endY = $(".side-box .contrast-img").offset().top;
 
-                            document.body.appendChild(cloneElement[0]);
-                            cloneElement.animate({
-                                top : endY,
-                                left : endX
-                            }, 500, function() {
-                                cloneElement.remove();
-                            });
+                                document.body.appendChild(cloneElement[0]);
+                                cloneElement.animate({
+                                    top : endY,
+                                    left : endX
+                                }, 500, function() {
+                                    cloneElement.remove();
+                                });
 
-                            self.find("input").attr("contrastid", data.contrastId);
-                            return;
-                        }
-                        if(data.result == -1) {
-                            self.find(".contrast-waring").html("对比已添加！你不需要继续添加。").removeClass("hidden");
-                            self.find("input").attr("checked", 'true');
+                                self.find("input").attr("contrastid", data.contrastId);
+                                return;
+                            }
+                            if(data.result == -1) {
+                                self.find(".contrast-waring").html("对比已添加！你不需要继续添加。").removeClass("hidden");
+                                self.find("input").attr("checked", 'true');
+                                window.setTimeout(function() {
+                                    self.find(".contrast-waring").addClass("hidden");
+                                }, 2000);
+                                return;
+                            }
+                            if(data.result == 1) {
+                                $(this).find(".contrast-waring").html("对比栏已满！你可以删除不需要的车辆，再继续添加。").removeClass("hidden");
+                                window.setTimeout(function() {
+                                    self.find(".contrast-waring").addClass("hidden");
+                                }, 2000);
+                                return;
+                            }
+                            $(this).find(".contrast-waring").html("加入对比失败，请刷新页面。").removeClass("hidden");
                             window.setTimeout(function() {
                                 self.find(".contrast-waring").addClass("hidden");
                             }, 2000);
-                            return;
-                        }
-                        if(data.result == 1) {
-                            $(this).find(".contrast-waring").html("对比栏已满！你可以删除不需要的车辆，再继续添加。").removeClass("hidden");
-                            window.setTimeout(function() {
-                                self.find(".contrast-waring").addClass("hidden");
-                            }, 2000);
-                            return;
-                        }
-                        $(this).find(".contrast-waring").html("加入对比失败，请刷新页面。").removeClass("hidden");
-                        window.setTimeout(function() {
-                            self.find(".contrast-waring").addClass("hidden");
-                        }, 2000);
-                    });
+                        });
+                    } else {
+                        alert("数据不合法");
+                    }
                 } else {
-                    alert("数据不合法");
+                    if(!!contrastId) {
+                        $.ajax({
+                            type : "POST",
+                            url : config.deleteContrast,
+                            data : {
+                                cid : contrastId
+                            },
+                            dataType : "json",
+                            context : self
+                        }).done(function(data) {
+                            self.find("input").removeAttr("checked");
+                            self.find("input").removeAttr("contrastid");
+                        });
+                    }
                 }
-            } else {
-                if(!!contrastId) {
-                    $.ajax({
-                        type : "POST",
-                        url : config.deleteContrast,
-                        data : {
-                            cid : contrastId
-                        },
-                        dataType : "json",
-                        context : self
-                    }).done(function(data) {
-                        self.find("input").removeAttr("checked");
-                        self.find("input").removeAttr("contrastid");
-                    });
-                }
-            }
+            })
+
             return false;
         },
         subscibe: function(e) {
