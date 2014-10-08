bc3b5270d62b44db12962ac83c69b30499a61062
xinyu198736 <xinyu198736@gmail.com>
2014-10-08 15:37:50 +0800

    list浮动nav

--- assets/js/detail/report/report-important.js
+++ assets/js/detail/report/report-important.js
@@ -2,7 +2,7 @@ define(function(){
 
     return {
         init:function(){
-            var lastPic = $(".pics .pic-1");
+            var lastPic = $(".pics .pic-3");
             var active = function(index){
                 if(index=="pic-5"){
                     $(".pic-main").animate({
@@ -23,13 +23,26 @@ define(function(){
             }
             $(".trigger").on("mouseover",function(){
 //                $(".pics .pic").addClass("hidden");
+                clearInterval(timer);
                 var picname = $(this).attr("data-pic");
-                nowIndex = picname.replace(/^[0-9]/g,"")*1-1;
-                active(picname)
+                var index = picname.replace(/[^0-9]/g,"")*1;
+
+                active("pic-"+index);
+                for(var i=0;i<arr.length;i++){
+                    if(arr[i]==index){
+                        nowIndex = i;
+                        break;
+                    }
+                }
+            }).on("mouseleave",function(){
+                timer = setInterval(function(){
+                    nowIndex++
+                    active("pic-"+arr[(nowIndex%5)])
+                },3000)
             })
             var arr = [3,4,2,5,1]
             var nowIndex = 0;
-            setInterval(function(){
+            var timer = setInterval(function(){
                 nowIndex++
                 active("pic-"+arr[(nowIndex%5)])
             },3000)
