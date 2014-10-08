e2845cd16bf62796e50a8a74d479988703674400
xinyu198736 <xinyu198736@gmail.com>
2014-10-08 15:08:16 +0800

    list浮动nav

--- assets/js/detail/report/report-important.js
+++ assets/js/detail/report/report-important.js
@@ -2,16 +2,37 @@ define(function(){
 
     return {
         init:function(){
-            $(".trigger").on("mouseover",function(){
-                $(".pics .pic").addClass("hidden");
-                var picname = $(this).attr("data-pic");
-                if(picname=="pic-5"){
-                    $(".pic-main").addClass("hidden")
+            var lastPic = $(".pics .pic-1");
+            var active = function(index){
+                if(index=="pic-5"){
+                    $(".pic-main").animate({
+                        opacity:0
+                    })
                 }else{
-                    $(".pic-main").removeClass("hidden")
+                    $(".pic-main").animate({
+                        opacity:1
+                    })
                 }
-                $(".pics").find("."+picname).removeClass("hidden")
+                lastPic.animate({
+                    opacity:0
+                })
+                $(".pics").find("."+index).animate({
+                    opacity:1
+                })
+                lastPic = $(".pics").find("."+index)
+            }
+            $(".trigger").on("mouseover",function(){
+//                $(".pics .pic").addClass("hidden");
+                var picname = $(this).attr("data-pic");
+                nowIndex = picname.replace(/^[0-9]/g,"")*1-1;
+                active(picname)
             })
+            var arr = [3,4,2,5,1]
+            var nowIndex = 0;
+            setInterval(function(){
+                nowIndex++
+                active("pic-"+arr[(nowIndex%5)])
+            },3000)
         }
     }
 })
