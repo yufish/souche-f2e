d75b628d1fa5876f74ca8d525e6897ca776a2b65
teamrun <chenllos@163.com>
2014-10-17 15:58:48 +0800

    提交r.js 去除缓存

--- assets/js/shop/index.r.js
+++ assets/js/shop/index.r.js
@@ -46,12 +46,19 @@ define('shop/index',['shop/shopMap.js'], function(ShopMap){
             _view.initShopMap();
         },
         initShopMap: function(){
-            ShopMap.init(config.shopLocation);
+            var coord = {};
+            
+            var loca = config.shopLocation.split(',');
+            // test data
+            // var loca = '116.417854, 39.921988'.split(',');
+            coord.log = Number(loca[0]);
+            coord.lat = Number(loca[1]);
+            ShopMap.init(coord);
         }
     };
 
     function init(){
-        $.extend(config, pageConfig);
+        $.extend(config, shopConfig||{});
 
         _view.init();
     }
