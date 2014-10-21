fae40ddad77ad36cd2f38c5641169c1579483e09
zilong <jzlxiaohei@163.com>
2014-10-21 13:10:05 +0800

    login with valid code

--- assets/js/mobile/common/loginUtil.js
+++ assets/js/mobile/common/loginUtil.js
@@ -32,7 +32,8 @@ window.SM=window.SM||{};
 //检查是否填过手机号
 SM.checkPhoneExist = function(callback) {
     $.ajax({
-        url: contextPath + "/pages/evaluateAction/isNoRegisterLogin.json",
+        //url: contextPath + "/pages/evaluateAction/isNoRegisterLogin.json",
+        url: contextPath + "/pages/evaluateAction/isPhoneLogin.json",
         type: "post",
         dataType: "json",
         success: function(data) {
@@ -69,13 +70,28 @@ SM.PhoneRegister = function(phone, callback) {
     })
 };
 
+//cellphone,token,time
+SM.SendValidCode = function(data){
+    $.ajax({
+        dataType:'json',
+        url:contextPath+'/pages/smsCaptchaAction/send.json',
+        data:data,
+        success:function(e){
+            if(e.errorMessage){
+                alert('发生错误,请一分钟后重试')
+            }
+        }
+    })
+}
+
 //带验证码的弹出框
-function createLoginWithValid(token){
+function createLoginWithValid(token,time){
     var phoneNum = checkUserLocal().phoneNum;
     if(phoneNum){
         var partial='<div id="J_phone_valid" class="user-phone-num">您的手机号:'+phoneNum+'</div>'
+                    + '<input id="J_phone-valid" name="cellphone" class="user-phone-num" type="hidden" value="'+phoneNum+'">'
     }else{
-       var partial='<input id="J_phone-valid" class="login-input phone-input" type="tel" placeholder="输入手机号">'
+       var partial='<input id="J_phone-valid" name="cellphone" class="login-input phone-input" type="tel" placeholder="输入手机号">'
     }
     var html = '<div class="login-area hidden" id="login-form-valid-code">'
         +'<form id="login-form-valid">'
@@ -87,48 +103,55 @@ function createLoginWithValid(token){
         +        '<input id="J_valid-code" type="tel" class="valid-code-input login-input" placeholder="输入验证码">'
         +        '</div>'
         +        '<div class="item-1-2">'
-        +            '<button type="button" class="valid-code-btn" id="J_get-valid">获取手机验证码</button>'
+        +            '<button type="button" class="valid-code-btn" name="cellphone" id="J_get-valid">获取手机验证码</button>'
         +        '</div>'
         +    '</div>'
         +    '<div class="form-item">'
         +        '<button type="submit" class="login-btn" id="J_login-valid">登录</button>'
         +    '</div>'
-        +    '<input type="hidden" name="token" value="'+token+'">'
+        +    '<input type="hidden" name="token" id="token-input" value="'+token+'">'
+        +    '<input type="hidden" name="time" id="time-input" value="'+time+'">'
         +'</form>'
         +'</div>'
     $(html).appendTo('body');
     var phoneReg = /^1[3458][0-9]{9}$/;
-    function checkPhone(){
-        var phoneNum = $('#J_phone-valid').val();
-        return phoneReg.test(phoneNum)
-    }
-    $('#J_login-valid').click(function(){
-        var phoneNum = $('#J_phone-valid').val();
-        if(!checkPhone()){
-            e.preventDefault();
-            alert('请输入正确的手机号')
-            return;
+    !function(){
+        function checkPhone(){
+            var phoneNum = $('#J_phone-valid').val();
+            return phoneReg.test(phoneNum)
         }
-        var validCode = $("#J_valid-code").val();
-    })
-    $('#J_get-valid').click(function(){
-        if(!checkPhone()){
-            alert('请输入正确的手机号')
-            return;
-        }
-        var time = 60;
-        var self = $(this).text('xx '+time+'s xx').prop({disabled:true});
-        var handle = setInterval(function(){
-            time--;
-            if(time==0){
-                self.text('获取手机验证码').prop({disabled:false});
-                clearInterval(handle);
 
-            }else{
-                self.text('xx '+time+'s xx');
+        $('#J_login-valid').click(function(){
+            var phoneNum = $('#J_phone-valid').val();
+            if(!checkPhone()){
+                e.preventDefault();
+                alert('请输入正确的手机号')
+                return;
             }
-        },1000)
-    })
+            var validCode = $("#J_valid-code").val();
+        })
+
+
+        $('#J_get-valid').click(function(){
+            if(!checkPhone()){
+                alert('请输入正确的手机号')
+                return;
+            }
+            SM.SendValidCode($('#login-form-valid').serialize())
+            var time = 60;
+            var self = $(this).text(time+'秒后可重发').prop({disabled:true});
+            var handle = setInterval(function(){
+                time--;
+                if(time==0){
+                    self.text('获取手机验证码').prop({disabled:false});
+                    clearInterval(handle);
+                }else{
+                    self.text(time+'秒后可重发');
+                }
+            },1000)
+        })
+    }()
+
 
 }
 
