fbe0e47695569c9b0600a674fee36652a7b9dc7a
teamrun <chenllos@163.com>
2014-10-27 19:40:53 +0800

    car id 输入框 和 为此打基础的toolbar系统

--- assets/js/chat/component/Compose.react.js
+++ assets/js/chat/component/Compose.react.js
@@ -14,25 +14,44 @@
 
 var React = require('react');
 var ComposeInput = require('./ComposeInput.react.js');
+var ComposeCarid = require('./ComposeTool-Carid.react.js');
 
 var Compose = React.createClass({
     propTypes: {
         textsHandler: React.PropTypes.func.isRequired
     },
+    componentDidMount: function(){
+        // 由toolbar负责把所有的tool隐藏
+        //$(document.body).on('click', function(){
+        //    var allTools = $('.compose-tool');
+        //    allTools.removeClass('active');
+        //});
+        //$(this.getDOMNode()).on('click', function(e){
+        //    // 一旦使用jquery的事件就会吧react的事件逻辑破坏掉...
+        //    e.stopPropagation();
+        //});
+    },
 
     render: function() {
-        // <div className="toolbar">
-        //             <div className="tool-demo" onClick={this._insertText}>哈哈</div>
-        //         </div>
+
         return (
+            
             <div className="compose-ctn"  style={this.props.style}>
-                
+                <div className="compose-toolbar" onClick={this._noBubble}>
+                    <ComposeCarid />
+                </div>
                 <ComposeInput submitHandler={this.submitText}/>
             </div>
         );
     },
     submitText: function(text){
         this.props.textsHandler(text);
+    },
+    _noBubble: function(e){
+        // 点击到toolbar之内不要隐藏...
+        // 貌似无效啊...
+        // 是因为绑在body上的和绑在这里的事件不是一个体系的吗```
+        e.stopPropagation();
     },
     _insertText: function(){
 
