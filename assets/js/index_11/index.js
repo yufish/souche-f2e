38764cd05c144f7a33e5ddd59e550a0f2087f93d
teamrun <chenllos@163.com>
2014-11-18 10:30:14 +0800

    限时优惠的动效

--- assets/js/index_11/index.js
+++ assets/js/index_11/index.js
@@ -1,6 +1,11 @@
 define(function(){
-    var Index = {
-        init:function(){
+
+    var _view = {
+        init: function(){
+            _view.initChooseNav();
+            _view.initLimitSaleHover();
+        },
+        initChooseNav: function(){
             $("#choose_brand").on("mouseenter",function(){
                 $(this).addClass("active");
             }).on("mouseleave",function(){
@@ -11,6 +16,22 @@ define(function(){
             }).on("mouseleave",function(){
                 $(this).removeClass("active");
             })
+        },
+        initLimitSaleHover: function(){
+            // 鼠标进入bubble 和 图片时触发
+            var triggerEl = $('.limit-sale .limit-info, .limit-sale .car-info .img-ctn');
+            var affectEl = $('.limit-sale');
+            triggerEl.on('mouseenter', function(){
+                affectEl.addClass('hover');
+            }).on('mouseleave', function(){
+                affectEl.removeClass('hover');
+            });
+        }
+    }
+
+    var Index = {
+        init:function(){
+            _view.init();
         }
     }
     return Index;
