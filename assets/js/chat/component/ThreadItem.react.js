c6104e996109bbcf1da69c34f18fe5f3bcbe3362
teamrun <chenllos@163.com>
2014-11-18 18:59:45 +0800

    在threadItem上增加查看用户信息的btw

--- assets/js/chat/component/ThreadItem.react.js
+++ assets/js/chat/component/ThreadItem.react.js
@@ -4,6 +4,22 @@
 
 var React = require('react');
 
+var friendIdPrefix = 'buyer_';
+
+var contextPath = contextPath || '';
+function openCrmSalerInfo(user, name){
+    if(user==undefined) {
+        user = "";
+        name = "新增卖家用户";
+    }
+
+    user = user.replace(/ /g, "");
+    
+    var url = contextPath + "/pages/crm/crm_saler_info.html?user=" + user;
+    //var url = contextPath + "/pages/crm/usercard/user_card.html?userid=" + user;还是用老页面，卖家
+    parent.addTab(user,"卖家-" + name + "的信息",url,tabId);
+}
+
 var ThreadItem = React.createClass({
     render: function() {
         //var users = this.props.thread.members.map(function(u){
@@ -22,6 +38,7 @@ var ThreadItem = React.createClass({
                 onClick={this._onClick}
             >
                 <span className="thread-name">{ threadName }</span>
+                <span className="icon icon-info friend-info-btn" onClick={this.openFriendInfo}></span>
                 <span className="thread-last-contact">{ lastContact }</span>
             </li>
         );
@@ -30,6 +47,10 @@ var ThreadItem = React.createClass({
         // 不要用e来获取当前el的数据属性
         // 可以直接用this.props哦~
         this.props.itemClickHandler(this.props.thread.id);
+    },
+    openFriendInfo: function(){
+        var crmId = this.props.thread.friendId.substr(friendIdPrefix.length);
+        openCrmSalerInfo(crmId, '');
     }
 });
 
