b3c2fa3676b1614d6ee11f6c27d40efe8842b3f4
mizhengdi <mizhdi@gmail.com>
2015-01-12 14:34:06 +0800

    fix

--- assets/js/mobile/slide/slide.js
+++ assets/js/mobile/slide/slide.js
@@ -110,6 +110,7 @@ $(function() {
         action.goto(7);
         $('.section-form').removeClass('hidden');
         $('.section-result').addClass('hidden');
+        $(this).attr('data-num', '0').html('举手之劳 传递亲情');
       }
     });
 
