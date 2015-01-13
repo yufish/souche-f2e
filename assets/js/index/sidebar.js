b4b3982a136b2d70bd36de91a4d8b6818bac9887
xinyu198736 <xinyu198736@gmail.com>
2015-01-13 17:38:34 +0800

    聊天

--- assets/js/index/sidebar.js
+++ assets/js/index/sidebar.js
@@ -71,11 +71,7 @@ Souche.Sidebar = (function() {
 
         $("#talk_with").on("click",function(e){
             var uid = $(this).attr("data-userid");
-            if(Math.random()>0.5){
-                uid = "cn_18667046361"
-            }else{
-                uid = "cn_17098045671"
-            }
+            
             Souche.Sidebar.showTalk(uid,window.location.href);
         })
         $(".advisor-tip-close").click(function(e) {
